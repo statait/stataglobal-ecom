4999c176968bc8591932e6c0668201e3675430e7
Fahmid <fahmidh26@gmail.com>
2024-02-07 14:53:45 +0600

    Bulb Enter Done Fahmid

--- resources/views/frontend/main_master.blade.php
+++ resources/views/frontend/main_master.blade.php
@@ -407,6 +407,7 @@ function productView(id){
             $('#product_id').val(id);
             $('#qty').val(1);
 
+
             // Product Price 
             if (data.product.discount_price == null) {
                 $('#pprice').text('');
@@ -443,6 +444,22 @@ function productView(id){
             $('#sizeArea').show();
         }
     }) // end size
+
+// Disable increment button and restrict quantity if product ID is 119
+if (id == 119) {
+                $('#qty').attr('max', 1);
+                $('#qty').attr('readonly', true);
+                $('.btn-number[data-type="plus"]').prop('disabled', true);
+            } else {
+                $('#qty').removeAttr('max');
+                $('#qty').removeAttr('readonly');
+                $('.btn-number[data-type="plus"]').prop('disabled', false);
+            }
+            
+            // Reset quantity to 1 for all products
+            $('#qty').val(1);
+
+
        }
    })
 
@@ -457,6 +474,7 @@ function productView(id){
         var color = $('#color option:selected').text();
         var size = $('#size option:selected').text();
         var quantity = $('#qty').val();
+        
         $.ajax({
             type: "POST",
             dataType: 'json',
@@ -719,13 +737,13 @@ function wishlistRemove(id){
           `<strong>${value.options.size} </strong>` 
           }           
             </td>
-           <td class="col-md-2">
+            <td class="col-md-2">
             ${value.qty > 1
             ? `<button type="submit" class="btn btn-danger btn-sm" id="${value.rowId}" onclick="cartDecrement(this.id)" >-</button> `
             : `<button type="submit" class="btn btn-danger btn-sm" disabled >-</button> `
             }
         <input type="text" value="${value.qty}" min="1" max="100" disabled="" style="width:25px;" >  
-         <button type="submit" class="btn btn-success btn-sm" id="${value.rowId}" onclick="cartIncrement(this.id)" >+</button> 
+         <button type="submit" class="btn btn-success btn-sm" id="${value.rowId}" onclick="cartIncrement(this.id)" ${value.id === '119' ? 'disabled' : ''}>+</button> 
             </td>
              <td class="col-md-2">
             <strong>TK ${value.subtotal} </strong> 
