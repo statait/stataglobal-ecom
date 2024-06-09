9399e8d6be4080486faa38dbbe3633deafd4bf4d
ifazalam1 <ifaz.alam1@gmail.com>
2024-06-09 10:58:47 +0600

    testimonial kowsheek

--- resources/views/frontend/common/testomonials.blade.php
+++ resources/views/frontend/common/testomonials.blade.php
@@ -1,9 +1,9 @@
 <div id="myhidemobile" class="sidebar-widget  wow fadeInUp outer-top-vs ">
   <div id="advertisement" class="advertisement">
     <div class="item">
-      <div class="avatar"><img src="{{ asset('frontend/assets/images/testimonials/member2.jpg') }}" alt="member1"></div>
-      <div class="testimonials"><em>"</em> My Name is Murgiiiiiiii!!<em>"</em></div>
-      <div class="clients_author">Murgi Chor<span></span> </div>
+      <div class="avatar"><img src="{{ asset('frontend/assets/images/testimonials/member1.png') }}" alt="member1"></div>
+      <div class="testimonials"><em>"</em> STATA delivers smart, secure, stylish living with top-notch customer support!!<em>"</em></div>
+      <div class="clients_author">Lamia<span></span> </div>
       <!-- /.container-fluid --> 
     </div>
     <!-- /.item -->
