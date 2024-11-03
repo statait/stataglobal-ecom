1b1702835501ee66c08dfb8cf626a389f3235f25
ifazalam1 <ifaz.alam1@gmail.com>
2024-11-03 15:09:21 +0600

    google tag manager

--- resources/views/frontend/main_master.blade.php
+++ resources/views/frontend/main_master.blade.php
@@ -53,6 +53,13 @@ $seo = App\Models\Seo::find(1);
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer','GTM-M8XLGD7');</script>
 <!-- End Google Tag Manager -->
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-PTSL6CMK');</script>
+<!-- End Google Tag Manager -->
 
 
 <title>@yield('title') </title>
@@ -229,6 +236,11 @@ rel="stylesheet">
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M8XLGD7"
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PTSL6CMK"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 <!-- ============================================== HEADER ============================================== -->
 @include('frontend.body.header')
