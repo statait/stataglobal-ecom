ae006971fe2900fe7bfd7c339e8c5e2d83988b8e
ifazalam1 <ifaz.alam1@gmail.com>
2024-11-05 13:30:31 +0600

    Sale form renamed to 11.11

--- resources/views/frontend/product/sale_view.blade.php
+++ resources/views/frontend/product/sale_view.blade.php
@@ -172,7 +172,7 @@ Product List
             @if (request()->routeIs('combo.offer'))
             <h1>Combo Package Offer</h1>
             @elseif (request()->routeIs('sale.offer'))
-                <h1>Sale</h1>
+                <h1>11.11</h1>
             @endif
             <!-- /.col -->
             {{-- <div class="col col-sm-12 col-md-6">
