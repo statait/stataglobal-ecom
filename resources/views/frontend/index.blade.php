3805baa0cc115eaab0f14b291be68c3922bc0f96
ifazalam1 <ifaz.alam1@gmail.com>
2024-11-16 13:29:03 +0600

    in new product category removed

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -535,15 +535,7 @@ STATA IT LIMITED | Smart Home Automation In Bangladesh
 		  <div id="product-tabs-slider" class="scroll-tabs outer-top-vs wow fadeInUp">
 			<div class="more-info-tab clearfix ">
 			  <h3 class="new-product-title pull-left">New Products</h3>
-			  <ul class="nav nav-tabs nav-tab-line pull-right" id="new-products-1">
-				<li class="active"><a data-transition-type="backSlide" href="#all" data-toggle="tab">All</a></li>
-				@foreach($categories as $category)
-				<li><a data-transition-type="backSlide" href="#category{{ $category->id }}" data-toggle="tab">{{ $category->category_name }}</a></li>
-				@endforeach
-				{{-- <li><a data-transition-type="backSlide" href="#smartphone" data-toggle="tab">Clothing</a></li>
-				<li><a data-transition-type="backSlide" href="#laptop" data-toggle="tab">Electronics</a></li>
-				<li><a data-transition-type="backSlide" href="#apple" data-toggle="tab">Shoes</a></li> --}}
-			  </ul>
+			  
 			  <!-- /.nav-tabs --> 
 			</div>
 			<div class="tab-content outer-top-xs">
